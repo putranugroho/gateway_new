07e4d41a20c3a256b3f29e80cffef8aa8c8e2d1c
snooters <cybershane11@gmail.com>
2024-01-13 10:47:26 +0700

    commit

---
+++ controller/cms/echo.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const Validator = require('fastest-validator');
+const { validateApiKey } = require('../../utils/validateApiKey');
+const { printreq, printres } = require('../../utils/getprint');
+const { callAPI } = require('../../utils/execAPI')
+const v = new Validator();
+const { CORE_URL } = process.env
+
+
+
+router.post('/coreecho', validateApiKey, async (req, res) => {
+    let response = {}
+    let header = {
+
+    }
+    printreq(req.body, "ECHO CORE");
+    response = await callAPI(CORE_URL, "echo", req.body, header)
+    printres(response, "ECHO CORE");
+    res.status(200).send(response);
+});
+
+module.exports = router
--- router/transaksi.js
+++ router/transaksi.js
@@ -10,11 +10,14 @@ const glrouter = require('../controller/cms/gl')
 const logrouter = require('../controller/cms/logs')
 const updaterouter = require('../controller/cms/update')
 const ppobrouter = require('../controller/cms/ppob')
+const echorouter = require('../controller/cms/echo')
+
 router.use('/inquiry', useridrouter)
 router.use('/mpin', mpinrouter)
 router.use('/gl', glrouter)
 router.use('/log', logrouter)
 router.use('/update', updaterouter)
 router.use('/ppob', ppobrouter)
+router.use('/echo', echorouter)
 
 module.exports = router
