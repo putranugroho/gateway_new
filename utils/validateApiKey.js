d48998a6ac4b921c0f2bb78aa1461d5009e30295
snooters <cybershane11@gmail.com>
2024-01-27 19:24:25 +0700

    co

--- utils/validateApiKey.js
+++ utils/validateApiKey.js
@@ -1,7 +1,7 @@
 require('dotenv').config;
 async function validateApiKey(req, res, next) {
     const apiKey = req.headers['api-key'];
-    console.log(apiKey);
+    console.log(req.headers);
     if (apiKey !== process.env.API_KEY) {
         return res.status(401).json({ error: 'you not authorized to use this service' });
     }
