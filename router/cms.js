b7abc9474babea776184ff76a1bcedbaf7d49d8e
snooters <cybershane11@gmail.com>
2024-03-27 12:05:39 +0700

    commit

--- router/cms.js
+++ router/cms.js
@@ -8,11 +8,13 @@ const useridrouter = require('../controller/cms/userid')
 const mpinrouter = require('../controller/cms/mpin_cms')
 const kantorrouter = require('../controller/cms/kantor')
 const akunrouter = require('../controller/cms/akun')
+const acctyperouter = require('../controller/cms/accttype')
 
 router.use('/userid', useridrouter)
 router.use('/mpin', mpinrouter)
 router.use('/kantor', kantorrouter)
 router.use('/akun', akunrouter)
+router.use('/acctype', acctyperouter)
 
 
 
